16023a409bc3cd74a94f432dcfc262e7e677f9de
maher-alzanaty <mhr.zanaty@gmail.com>
2026-03-11 08:03:33 -0700

    ready for Render deploy

--- src/server.js
+++ src/server.js
@@ -30,5 +30,5 @@ app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use("/methods", methodRoutes);
 app.use("/payments", paymentRoutes);
 app.use("/admins", adminRoutes);
-
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
