7c2b22231bca8e4bf4262054f53b7446b3517a69
maher-alzanaty <mhr.zanaty@gmail.com>
2026-03-14 03:41:41 -0700

    ready for Render deploy55

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -39,7 +39,7 @@ router.post("/login", async (req, res) => {
     .cookie("token", token, {
       httpOnly: true,
       secure: "false",
-      sameSite:"lax",
+      sameSite:"none",
       path: "/",
       maxAge: 24 * 60 * 60 * 1000,
     })
